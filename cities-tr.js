8b45e732b4759d8277ccf1af5b51f1d8184113fb
umurakbulut <umur.akbulut@ingamegroup.com>
2020-12-17 04:15:39 +0300

    Added Turkey cities javascript array.

---
+++ cities-tr.js
@@ -0,0 +1,326 @@
+const cities = [
+    {
+        licensePlate: "01",
+        name: "Adana"
+    },
+    {
+        licensePlate: "02",
+        name: "Adıyaman"
+    },
+    {
+        licensePlate: "03",
+        name: "Afyonkarahisar"
+    },
+    {
+        licensePlate: "04",
+        name: "Ağrı"
+    },
+    {
+        licensePlate: "05",
+        name: "Amasya"
+    },
+    {
+        licensePlate: "06",
+        name: "Ankara"
+    },
+    {
+        licensePlate: "07",
+        name: "Antalya"
+    },
+    {
+        licensePlate: "08",
+        name: "Artvin"
+    },
+    {
+        licensePlate: "09",
+        name: "Aydın"
+    },
+    {
+        licensePlate: "10",
+        name: "Balıkesir"
+    },
+    {
+        licensePlate: "11",
+        name: "Bilecik"
+    },
+    {
+        licensePlate: "12",
+        name: "Bingöl"
+    },
+    {
+        licensePlate: "13",
+        name: "Bitlis"
+    },
+    {
+        licensePlate: "14",
+        name: "Bolu"
+    },
+    {
+        licensePlate: "15",
+        name: "Burdur"
+    },
+    {
+        licensePlate: "16",
+        name: "Bursa"
+    },
+    {
+        licensePlate: "17",
+        name: "Çanakkale"
+    },
+    {
+        licensePlate: "18",
+        name: "Çankırı"
+    },
+    {
+        licensePlate: "19",
+        name: "Çorum"
+    },
+    {
+        licensePlate: "20",
+        name: "Denizli"
+    },
+    {
+        licensePlate: "21",
+        name: "Diyarbakır"
+    },
+    {
+        licensePlate: "22",
+        name: "Edirne"
+    },
+    {
+        licensePlate: "23",
+        name: "Elazığ"
+    },
+    {
+        licensePlate: "24",
+        name: "Erzincan"
+    },
+    {
+        licensePlate: "25",
+        name: "Erzurum"
+    },
+    {
+        licensePlate: "26",
+        name: "Eskişehir"
+    },
+    {
+        licensePlate: "27",
+        name: "Gaziantep"
+    },
+    {
+        licensePlate: "28",
+        name: "Giresun"
+    },
+    {
+        licensePlate: "29",
+        name: "Gümüşhane"
+    },
+    {
+        licensePlate: "30",
+        name: "Hakkari"
+    },
+    {
+        licensePlate: "31",
+        name: "Hatay"
+    },
+    {
+        licensePlate: "32",
+        name: "Isparta"
+    },
+    {
+        licensePlate: "33",
+        name: "Mersin"
+    },
+    {
+        licensePlate: "34",
+        name: "İstanbul"
+    },
+    {
+        licensePlate: "35",
+        name: "İzmir"
+    },
+    {
+        licensePlate: "36",
+        name: "Kars"
+    },
+    {
+        licensePlate: "37",
+        name: "Kastamonu"
+    },
+    {
+        licensePlate: "38",
+        name: "Kayseri"
+    },
+    {
+        licensePlate: "39",
+        name: "Kırklareli"
+    },
+    {
+        licensePlate: "40",
+        name: "Kırşehir"
+    },
+    {
+        licensePlate: "41",
+        name: "Kocaeli"
+    },
+    {
+        licensePlate: "42",
+        name: "Konya"
+    },
+    {
+        licensePlate: "43",
+        name: "Kütahya"
+    },
+    {
+        licensePlate: "44",
+        name: "Malatya"
+    },
+    {
+        licensePlate: "45",
+        name: "Manisa"
+    },
+    {
+        licensePlate: "46",
+        name: "Kahramanmaraş"
+    },
+    {
+        licensePlate: "47",
+        name: "Mardin"
+    },
+    {
+        licensePlate: "48",
+        name: "Muğla"
+    },
+    {
+        licensePlate: "49",
+        name: "Muş"
+    },
+    {
+        licensePlate: "50",
+        name: "Nevşehir"
+    },
+    {
+        licensePlate: "51",
+        name: "Niğde"
+    },
+    {
+        licensePlate: "52",
+        name: "Ordu"
+    },
+    {
+        licensePlate: "53",
+        name: "Rize"
+    },
+    {
+        licensePlate: "54",
+        name: "Sakarya"
+    },
+    {
+        licensePlate: "55",
+        name: "Samsun"
+    },
+    {
+        licensePlate: "56",
+        name: "Siirt"
+    },
+    {
+        licensePlate: "57",
+        name: "Sinop"
+    },
+    {
+        licensePlate: "58",
+        name: "Sivas"
+    },
+    {
+        licensePlate: "59",
+        name: "Tekirdağ"
+    },
+    {
+        licensePlate: "60",
+        name: "Tokat"
+    },
+    {
+        licensePlate: "61",
+        name: "Trabzon"
+    },
+    {
+        licensePlate: "62",
+        name: "Tunceli"
+    },
+    {
+        licensePlate: "63",
+        name: "Şanlıurfa"
+    },
+    {
+        licensePlate: "64",
+        name: "Uşak"
+    },
+    {
+        licensePlate: "65",
+        name: "Van"
+    },
+    {
+        licensePlate: "66",
+        name: "Yozgat"
+    },
+    {
+        licensePlate: "67",
+        name: "Zonguldak"
+    },
+    {
+        licensePlate: "68",
+        name: "Aksaray"
+    },
+    {
+        licensePlate: "69",
+        name: "Bayburt"
+    },
+    {
+        licensePlate: "70",
+        name: "Karaman"
+    },
+    {
+        licensePlate: "71",
+        name: "Kırıkkale"
+    },
+    {
+        licensePlate: "72",
+        name: "Batman"
+    },
+    {
+        licensePlate: "73",
+        name: "Şırnak"
+    },
+    {
+        licensePlate: "74",
+        name: "Bartın"
+    },
+    {
+        licensePlate: "75",
+        name: "Ardahan"
+    },
+    {
+        licensePlate: "76",
+        name: "Iğdır"
+    },
+    {
+        licensePlate: "77",
+        name: "Yalova"
+    },
+    {
+        licensePlate: "78",
+        name: "Karabük"
+    },
+    {
+        licensePlate: "79",
+        name: "Kilis"
+    },
+    {
+        licensePlate: "80",
+        name: "Osmaniye"
+    },
+    {
+        licensePlate: "81",
+        name: "Düzce"
+    },
+];
